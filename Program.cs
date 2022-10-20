6cbfaeeb64a540fc76837b3d373d7787976c22ea
Jovann Contreras <jcontreras@codestack.co>
2022-10-20 14:49:41 -0700

    comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿
+﻿// Jovann Contreras
+// Date 10/17/2022
+// Redid mini challenge 1 saying hello with data validation in the while loop and string.
+// Trying to validate a string, got the validation for play again working
+
+
 Console.Clear();
 string playAgain = "yes";
 while (playAgain == "yes")
